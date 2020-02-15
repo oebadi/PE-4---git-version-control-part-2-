f0259d3f45c0b02b2a467513c7ad71d76652c1fe
oebadi <43624739+oebadi@users.noreply.github.com>
2020-02-14 20:44:19 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,6 +3,15 @@ void *CreateBoard(){
     return *mat;
 }
 
+void DisplayBoard(int A[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+            std::cout << " " << B[i][j] <<" ";
+		}
+		std::endl;
+	}
+}
+
 int main(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
     
